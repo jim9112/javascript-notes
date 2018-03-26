5f516850768f2b571fedc9d096c2b9f62087b7bf
jim9112 <jim9112@gmail.com>
2018-03-26 19:11:37 -0400

    added notes to keyless car

--- keylessCar.js
+++ keylessCar.js
@@ -1,8 +1,10 @@
-var age = prompt("How old are you?");
-if (age > 18) {
-  alert("Powering on. Enjoy the ride!")
-} else if (age === 18) {
-  alert("Congratulations on your first year of driving. Enjoy the ride")
-} else {
-  alert("Sorry, you are too young to drive. Powering off.")
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+	alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+	alert("Powering On. Enjoy the ride!");
+} else if (Number(age) === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
 }
+// must follow this flow if the === apears second will not get the right answer
