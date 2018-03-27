ce5318564cabda7606abc3d146455f2bfbbb9611
jim9112 <jim9112@gmail.com>
2018-03-27 13:28:08 -0400

    added index and expanded keyless
    call js files in index to test in browser

--- keylessCar.js
+++ keylessCar.js
@@ -1,10 +1,21 @@
+function checkDriverAge() {
 var age = prompt("What is your age?");
-
 if (Number(age) < 18) {
-	alert("Sorry, you are too yound to drive this car. Powering off");
+	alert("Sorry, you are too young to drive this car. Powering off");
 } else if (Number(age) > 18) {
 	alert("Powering On. Enjoy the ride!");
 } else if (Number(age) === 18) {
 	alert("Congratulations on your first year of driving. Enjoy the ride!");
 }
+}
+function checkDriversAge2(a) {
+	if (Number(a) < 18) {
+	return alert("Sorry, you are too young to drive this car. Powering off");
+	} else if (Number(a) > 18) {
+		return alert("Powering On. Enjoy the ride!");
+	} else if (Number(a) === 18) {
+		return alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+}
+
 // must follow this flow if the === apears second will not get the right answer
