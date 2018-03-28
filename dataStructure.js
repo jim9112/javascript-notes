2388789977baa5913f6b2204dc3a1102223dec24
jim9112 <jim9112@gmail.com>
2018-03-28 13:58:03 -0400

    Minor Changes

--- dataStructure.js
+++ dataStructure.js
@@ -14,11 +14,13 @@ console.log(array);
 array.shift();
 console.log(array);
 array.sort()
+console.log(array);
 array.push("Kiwi");
 console.log(array);
 array.splice(0, 1);
 console.log(array);
 array.sort();
+console.log(array);
 array.reverse();
 console.log(array);
 
