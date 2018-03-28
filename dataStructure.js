ae4d70d978b3133cf0faf37a18eec1de9417f6a2
jim9112 <jim9112@gmail.com>
2018-03-28 10:49:10 -0400

    Added excersizes/ took notes

--- dataStructure.js
+++ dataStructure.js
@@ -1,1 +1,22 @@
-var list = ["tiger", "cat", "bear", "bird"];
+// var list = ["tiger", "cat", "bear", "bird"];
+// console.log(list[0]);
+// list.push("elephant");
+// console.log(list);
+// list.push(prompt("input animal"));
+// console.log(list);
+// var newList = list.concat(["mouse", "hampster"]);
+// console.log(newList);
+// var yetAnotherList = list.pop();
+// console.log(yetAnotherList);
+// console.log(list);
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.shift();
+console.log(array);
+array.sort()
+array.push("Kiwi");
+console.log(array);
+array.splice(0, 1);
+array.sort();
+array.reverse();
+console.log(array);
+
