2d01eeac74cace616aa380c90bca335570d567dc
jim9112 <jim9112@gmail.com>
2018-03-29 18:16:05 -0400

    more notes

--- fake facebook/script.js
+++ fake facebook/script.js
@@ -19,3 +19,15 @@ let newsfeed = [{
     userName: "Jhnon",
     timeline: "Insert meme here."
 }]
+
+var usernamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+    if (user === database[0].userName && pass === database[0].password) {
+console.log(newsfeed);
+    } else {
+        alert("Sorry, wrong username and/or password!!!")
+    }
+}
+signIn(usernamePrompt, passwordPrompt);
