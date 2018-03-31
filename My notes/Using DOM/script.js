04a6cbf8e67982b4ab44724189e2e770d77857ca
jim9112 <jim9112@gmail.com>
2018-03-31 15:55:01 -0400

    using DOM added

--- My notes/Using DOM/script.js
+++ My notes/Using DOM/script.js
@@ -6,9 +6,28 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-button.addEventListener("click", function() {
-    // console.log("test")
+
+function inputLenth() {
+    return input.value.length;
+}
+
+function createListElement() {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
+    input.value = "";
+}
+
+button.addEventListener("click", function () {
+    if (inputLenth() > 0) {
+        createListElement();
+    }
+
+})
+
+input.addEventListener("keypress", function (event) {
+    if (inputLenth() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+
 })
